4cf8cb495cf1e56069e85b454d08947353a1925a
Mouarius <mouarius@gmail.com>
2020-09-20 17:10:21 +0200

    Initialisation de Vue Routeur

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
-<script>
-
-import NavBar from "./components/NavBar.vue"
-import ElementPhoto from "./components/ElementPhoto.vue"
-
-export default {
-  name: 'App',
-  components: {
-    NavBar, ElementPhoto
-  },
-  data(){
-    return{
-      image1:{
-        id:1,
-        source:"/images/photos/village-1.jpg",
-        alt:"Village de cases en Afrique."
-      } 
-    }
-  }
-}
-</script>
-
 <template>
   <div id="app">
-    <div id="main-window">
-      <NavBar />
-      <section id="defilement-photos">
-        <ElementPhoto :image="image1"></ElementPhoto>
-    </section>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
   </div>
 </template>
 
@@ -36,9 +13,20 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
-body{
-  margin: 0px;
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
